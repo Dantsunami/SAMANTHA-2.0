14ddd3c491b16f43a06ebe11744904dfec076609
A.L.C <daniellimaramos223@gmail.com>
2023-02-17 17:42:45 -0300

    Prestar atenção no buffer do Pyaudio
    Buffer de 2048 parece ok

--- main.py
+++ main.py
@@ -5,26 +5,31 @@ import os
 import pyaudio
 import pyttsx3
 import json
+import core
 
 # Sintese de Fala
 engine = pyttsx3.init()
 
 voices = engine.getProperty('voices')
-engine.setProperty('voice', voices[-2].id)
+engine.setProperty('voice', voices[0].id)
 
 def speak(text):
     engine.say(text)
     engine.runAndWait()
 
+
+    # Reonhecimento de Fala
+
 model = Model("model")
 rec = KaldiRecognizer(model, 16000)
 
 p = pyaudio.PyAudio()
-stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=16000)
+stream = p.open(format=pyaudio.paInt16, channels=1, rate=16000, input=True, frames_per_buffer=2048)
 stream.start_stream()
 
+# Loop do Reconhecimento de Fala
 while True:
-    data = stream.read(16000)
+    data = stream.read(2048)
     if len(data) == 0:
         break
     if rec.AcceptWaveform(data):
@@ -35,4 +40,6 @@ while True:
             text = result['text']
 
             print(text)
-            speak(text)
+
+            if text == 'que horas são' or text == 'me diga as horas':
+                speak(core.SystemInfo.get_time())
